0d89ea470dd6519364786a1bcf30073818b344a1
Kraken <warunamadushanka456@gmail.com>
2024-02-15 22:25:58 +0530

    tooltip enhancement

--- src/bs-tooltip.directive.ts
+++ src/bs-tooltip.directive.ts
@@ -1,12 +1,32 @@
-import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+} from '@angular/core';
 
 declare var bootstrap: any;
 
+interface TooltipConfig {
+  boundary: string;
+  delay: {
+    show: number;
+    hide: number;
+  };
+  animation: boolean;
+  html: boolean;
+  trigger: string;
+  placement: string;
+  customClass: string;
+}
 @Directive({
   standalone: true,
   selector: '[bsTooltip]',
 })
 export class BootstrapTooltipDirective implements AfterViewInit {
+  @Input('bsTooltip') type: string = '';
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit(): void {
@@ -16,16 +36,35 @@ export class BootstrapTooltipDirective implements AfterViewInit {
       'tooltip'
     );
 
-    const tooltip = new bootstrap.Tooltip(this.el.nativeElement, {
+    const ttConfig = this.getTooltipConfig();
+    switch (this.type) {
+      case 'success':
+        break;
+      case 'info':
+        break;
+      case 'warning':
+        break;
+      case 'error':
+        break;
+      default:
+        break;
+    }
+
+    return this.createTooltip(ttConfig);
+  }
+  getTooltipConfig() {
+    return {
       boundary: 'window',
       delay: { show: 400, hide: 200 },
       animation: true,
       html: true,
       trigger: 'hover',
       placement: 'top',
-      customClass: 'tooltip-error-custom',
-    });
+      customClass: 'tooltip-' + this.type,
+    };
+  }
 
-    return tooltip;
+  private createTooltip(tooltipConfig: TooltipConfig) {
+    return new bootstrap.Tooltip(this.el.nativeElement, tooltipConfig);
   }
 }
